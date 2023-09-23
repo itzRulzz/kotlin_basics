f8fc09ce995dfda88d5a1682d1c9bbf8959aad88
Стяжкин Матвей Евгеньевич <01-21@std.pkgh.edu>
2023-09-23 17:42:29 +0300

    feat: basics1

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,29 @@
+/*
+Создать приложение, которое подсчитывает количество подряд
+идущих одинаковых символов во введенной строке.
+На вход подается, например, строка AAADSSSRRTTHAAAA.
+На выходе получаем A3DS3R2T2HA4. То есть, если количество
+подряд идущих символов меньше двух, то мы не пишем единицу
+*/
+
+fun main() {
+    println("Введите строку: ")
+    val text = readln()
+
+    var text2 = ""
+    var count = 0
+
+    for (i in text.indices) {
+        val c = text[i]
+        count++
+
+        if ((i == maxOf(text.length) - 1) || (text[i + 1] != c)) {
+            text2 += c
+            if (count != 1)
+                text2 += count
+            count = 0
+        }
+    }
+
+    println(text2)
+}
