839f3c7a9c5a65d95075371a438d986439bb6b4f
Стяжкин Матвей Евгеньевич <01-21@pkgh.edu>
2023-11-13 10:40:50 +0300

    fix: string_splitting wip

--- src/main/kotlin/basics4.kt
+++ src/main/kotlin/basics4.kt
@@ -3,6 +3,8 @@
 Символами операции могут быть: / — деление, * — умножение, + — сложение, - — вычитание. Числа могут быть вещественными.
 Числа и знак операции разделяются между собой одним пробелом. Ввод производится в формате - ЧИСЛО1 ЧИСЛО2 ОПЕРАЦИЯ.
 */
+// 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
+
 package main.kotlin
 
 fun string_input() {
@@ -23,17 +25,19 @@ fun equation_calculation(equation: String){
 
 }
 
-fun string_splitting(equation: String){
-    var num1: String = ""
-    var num2: String = ""
-    val c: Char
+fun string_splitting(equation: String) {
+    val splitted_equation = equation.split(' ')
 
-    // split; when
+    var num1 = splitted_equation[0].toDouble()
+    var num2 = splitted_equation[1].toDouble()
+    var operation = splitted_equation[2]
+
+    return num1, num2, operation
 }
 
 fun main(){
     println("Введите арифмитическое выражение из двух чисел вида «ЧИСЛО1 ЧИСЛО2 ОПЕРАЦИЯ» (пробелы обязательны): ")
-    val equation = string_input()
+    val equation = string_input().toString()
 
     println("Ответ: $equation = ${equation_calculation(equation)}")
 }
